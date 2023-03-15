5ebcbc3477e6333353895bcefd8e216c75c10209
Q-Tify <55044295+Q-Tify@users.noreply.github.com>
2023-03-15 20:44:52 +0300

    Delete rollup.config.js

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
-
-export default {
-  input: 'src/ts/index.ts',
-  output: { file: 'dist/bundle.js' },
-  plugins: [terser(), typescript(), /* Other plugins */],
-};
